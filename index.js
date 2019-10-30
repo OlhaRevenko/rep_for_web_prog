b6c5505ffa2cc9cc07d729340e4aa601414abb57
OlhaRevenko <Olya1030345@gmail.com>
2019-10-30 15:57:33 +0200

    some grid

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var path = require('path');
+
+app.use(cors({
+    origin: '*'
+  }));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
@@ -8,6 +13,12 @@ app.get('/', function(req, res) {
 });
 app.get('/style.less', function(req, res) {
     res.sendFile(path.join(__dirname + '/style.less'));
+});
+app.get('/Semantic-UI-CSS-master/semantic.min.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/Semantic-UI-CSS-master/semantic.min.css'));
+});
+app.get('/Semantic-UI-CSS-master/semantic.min.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/Semantic-UI-CSS-master/semantic.min.js'));
 });
 /*//
 app.get('/request', function(req, res) {
